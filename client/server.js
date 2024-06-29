cfca4a08228c4293cd3ce8b2a310bbd6ff18f209
shahul85198 <124188392+shahul85198@users.noreply.github.com>
2024-06-29 17:45:22 +0000

    added

--- client/server.js
+++ client/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Registeruser = require('./model');
+const jwt = require('jsonwebtoken');
+const middleware = require('./middleware');
+const cors = require('cors');
 
 mongoose.connect("mongodb+srv://shahul123:shahul123@mernauth.gjvajxp.mongodb.net/?retryWrites=true&w=majority&appName=mernauth",
   {
@@ -41,6 +44,49 @@ app.post('/register', async (req, res) => {
         console.log(error)
         return res.status(500).send('Internal server error')
     }
+})
+
+app.post('/login', async (req, res) => {
+        try {
+            const {email, password} = req.body;
+            let exist = await Registeruser.findOne({email});
+            if (!exist) {
+              return res.status(400).send('User Not Found');
+            }
+            if (exist.password !== password) {
+              return res.status(400).send('Invalid credentials');
+            }
+            let payload = {
+              user: {
+                id : exist.id
+              }
+            }
+            jwt.sign(payload, 'jwtSecret', {expiresIn:3600000},
+              (err, token) => {
+                if (err) throw err;
+                return res.json({token})
+              }
+            )
+        }
+        catch (err) {
+          console.log(err);
+          return res.status(500).send('Server Error')
+        }
+   }
+)
+
+app.get('/myprofile', middleware, async(req, res) => {
+  try {
+      let exist = await Registeruser.findById(req.user.id);
+      if (!exist) {
+        return res.status(400).send('User not found')
+      }
+      res.json(exist);
+  }
+  catch(err) {
+    console.log(err);
+    return res.status(500).send('server error')
+  }
 })
 
 app.listen(5000, () => {
